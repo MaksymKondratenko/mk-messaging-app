4270239f457e3ac97232b4b91e5341cbba90a891
MaksymKondratenko <maxym.kondratenko@gmail.com>
2018-10-08 21:07:15 +0200

    adds listener

---
+++ src/main/java/com/maxymkondratenko/rabbitlistener/Listener.java
@@ -0,0 +1,19 @@
+package com.maxymkondratenko.rabbitlistener;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Component
+@Slf4j
+public class Listener {
+    @Getter
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+      log.info("Received message: [" + message + "].");
+      latch.countDown();
+    }
+}
